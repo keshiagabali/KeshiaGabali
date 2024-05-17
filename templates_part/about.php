a304f2e997b378b6100114d5681b0e26d5920e66
keshiagabali <150538341+keshiagabali@users.noreply.github.com>
2024-05-17 19:32:41 +0200

    About

---
+++ templates_part/about.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Template Name: About Page
+ *
+ * Ce modèle est utilisé pour afficher une page de about.
+ */
+?>
+
+<?php get_header(); ?>
+
+<?php
+/**
+ * Template Name: About Page
+ *
+ * Ce modèle est utilisé pour afficher une page de about.
+ */
+?>
+
+<?php get_header(); ?>
+
+<section id="about" class="about-cv">
+
+    <div class="container-cv">
+
+        <div class="header-cv">
+            <h1>Keshia Gabali</h1>
+            <h3>Développeuse/Intégratrice Wordpress</h3>
+        </div>
+
+        <div class="content-cv">
+
+            <div class="left-column-cv">
+                <h2>Contact</h2>
+                <p class="description">Email: keshia.dev@yahoo.com</p>
+                <p class="description">Paris, France</p>
+                
+                <h2>Education</h2>
+                <p class="description">Developpeur Wordpress - Openclassrooms</p>
+                <p class="description">Formation Technicienne Support - Consort Group</p>
+                <p class="description">BTS Management Commercial Opérationnel - CNED</p>
+                
+                <h2>Skills</h2>
+                <ul class="description">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>PHP</li>
+                    <li>WordPress</li>
+                    <li>Responsive</li>
+                    <li>Git</li>
+                    <li>MySQL</li>
+                </ul>
+            </div>
+
+            <div class="right-column-cv">
+                <p class="description">Développeuse front-end Wordpress débutante.<br>Passionnée par les nouvelles technologies, créative et autonome, ayant travaillé sur des projets variés.</p>
+                
+                <h2>Experience</h2>
+                <p class="description">Assistante E-Commerce - Velvet Imperial</p>
+                <ul class="description">
+                    <li>Gestion des ventes en ligne : mise à jour, gestion des stocks.</li>
+                    <li>Création de visuels attractifs pour les produits.</li>
+                    <li>Création d'un site e-commerce Wordpress</li>
+                    <li>Mise en place de campagnes promotionnelles pour augmenter les ventes.</li>
+                </ul>
+
+                <p class="description">Stage Développeuse Web - Velvet Imperial</p>
+                <ul class="description">
+                    <li>Collaboration avec une équipe pour la création de site web responsive en HTML, CSS et JavaScript.</li>
+                </ul>
+
+
+                <p class="description">Support Technique - Consort Group</p>
+                <ul class="description">
+                    <li>Assistance technique dans la résolution des problèmes informatiques pour les utilisateurs.</li>
+                    <li>Gestion des tickets d'assistance technique pour assurer leur résolution dans les délais impartis.</li>
+                    <li>Collaboration avec les équipes techniques pour garantir une assistance efficace et professionnelle.</li>
+                </ul>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<?php get_footer(); ?>
